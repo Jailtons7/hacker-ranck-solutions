2219e97762dc1d1cf141f0b010e55794a6fe8592
Jailton Silva <jss1@ifal.edu.br>
2024-05-01 14:24:26 -0300

    add a description to discard, remove and pop set elements problem

--- python/sets/set_operations.py
+++ python/sets/set_operations.py
@@ -1,3 +1,35 @@
+"""
+The task is to perform commands over a given set and, at the end,
+print the sum of the remaining elements.
+The commands will be pop, remove and discard.
+
+Input Format
+
+The first line contains integer n, the number of elements in the set s.
+The second line contains n space separated elements of set s.
+All the elements are non-negative integers, less than or equal to 9.
+The third line contains integer m, the number of commands.
+The next m lines contains either pop, remove and/or discard commands followed by their associated value.
+
+Sample Input
+9
+1 2 3 4 5 6 7 8 9
+10
+pop
+remove 9
+discard 9
+discard 8
+remove 7
+pop
+discard 6
+remove 5
+pop
+discard 5
+
+Sample Output
+4
+"""
+
 from typing import Set
 
 
