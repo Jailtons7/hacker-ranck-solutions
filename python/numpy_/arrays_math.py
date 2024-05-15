b6170d6b698426719f82f8f1561eade8b5a7a85b
Jailton Silva <jss1@ifal.edu.br>
2024-05-14 23:13:59 -0300

    add a description to array mathematics problem

--- python/numpy_/arrays_math.py
+++ python/numpy_/arrays_math.py
@@ -1,3 +1,28 @@
+"""
+You are given two integer arrays, A and B of dimensions NxM.
+Your task is to perform the following operations:
+
+    Add
+    Subtract
+    Multiply
+    Integer Division
+    Mod
+    Power
+
+Sample Input:
+1 4
+1 2 3 4
+5 6 7 8
+
+Sample Output:
+[[ 6  8 10 12]]
+[[-4 -4 -4 -4]]
+[[ 5 12 21 32]]
+[[0 0 0 0]]
+[[1 2 3 4]]
+[[    1    64  2187 65536]]
+
+"""
 import numpy as np
 
 
